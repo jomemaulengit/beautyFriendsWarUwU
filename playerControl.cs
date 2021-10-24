f69f678aec0a0fe016afd96d2cc66ebdeec65086
MAULEN <maulenadasmejose@gmail.com>
2021-10-23 22:20:33 -0300

    add camera switch

--- playerControl.cs
+++ playerControl.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class playerControl : MonoBehaviour
 {
     [SerializeField] Vector2 sensitibity;
+    [SerializeField] Vector3 cameraPosition = new Vector3(0, 0, 0);
+    [SerializeField] Vector3 cameraRotation = new Vector3(0, 0, 0);
     [SerializeField] float speed;
     [SerializeField] new Transform camera=null;
+    [SerializeField] Transform axis=null;
     [SerializeField] private bool cursorIsBlocked=true;
+    [SerializeField] private bool isFPS=false;
     private Vector2 rotation;
     private Rigidbody myrb;
     private float maxVarticalDegrees=90;
@@ -29,18 +33,50 @@ public class playerControl : MonoBehaviour
         );
         return input;
     }
+    private void fpsCamera(){
+        camera.localPosition = new Vector3(0,0,0);
+        Vector2 velocity = MouseLook()*sensitibity;
+        rotation += velocity*Time.deltaTime;
+        rotation.y = ClampVerticalAngles(rotation.y);
+        transform.localEulerAngles= new Vector3(0,rotation.x,0);
+        camera.localEulerAngles = new Vector3(-rotation.y,0,0);
+    }
+    private void switchCamera(Transform axis){
+        if(Input.GetKeyDown(KeyCode.C)){
+            isFPS=!isFPS;
+        }
+    }
+    private void thirdpersonCamera(){
+        // if(camera.localEulerAngles.x<10){
+        //     cameraPosition.z+=camera.localEulerAngles.x;
+        // }
+        camera.localPosition = cameraPosition;
+        camera.localEulerAngles = cameraRotation;
+        // Vector2 velocity = MouseLook()*sensitibity;
+        // rotation += velocity*Time.deltaTime;
+        // rotation.y = ClampVerticalAngles(rotation.y);
+        // transform.localEulerAngles= new Vector3(0,rotation.x,0);
+        // camera.localEulerAngles = new Vector3(-rotation.y,0,0);
+
+    }
+    
+    private void FixedUpdate(){
+        Control();
+        if(!isFPS){fpsCamera();}else{thirdpersonCamera();}
+    }
     private void Control(){
+        switchCamera(axis);
         if (Input.GetKey(KeyCode.W)){
-            myrb.velocity = new Vector3(0,0,speed);
+            myrb.AddRelativeForce(new Vector3(0,0,speed));
         }
         if (Input.GetKey(KeyCode.S)){
-            myrb.velocity = new Vector3(0,0,-speed);
+            myrb.AddRelativeForce(new Vector3(0,0,-speed));
         }
         if (Input.GetKey(KeyCode.D)){
-            myrb.velocity = new Vector3(0,0,speed);
+            myrb.AddRelativeForce(new Vector3(speed,0,0));
         }
         if (Input.GetKey(KeyCode.A)){
-            myrb.velocity = new Vector3(0,0,-speed);
+            myrb.AddRelativeForce(new Vector3(-speed,0,0));
         }
         if (Input.GetKeyDown(KeyCode.Tab)){
             if(cursorIsBlocked==true)
@@ -54,13 +90,5 @@ public class playerControl : MonoBehaviour
                 cursorIsBlocked=true;
               }
         }
-    }
-    private void FixedUpdate(){
-        Vector2 velocity = MouseLook()*sensitibity;
-        rotation += velocity*Time.deltaTime;
-        rotation.y = ClampVerticalAngles(rotation.y);
-        transform.localEulerAngles= new Vector3(0,rotation.x,0);
-        camera.localEulerAngles = new Vector3(-rotation.y,0,0);
-        Control();
     }
 }
